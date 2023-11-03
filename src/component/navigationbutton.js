b25ed41427e9a1cd067043f55594f7eb59bc721d
florent.rve <florent.rve@gmail.com>
2023-11-03 06:41:28 +0400

    first commit

--- src/component/navigationbutton.js
+++ src/component/navigationbutton.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const NavButton = () => {
   return (
-    <div className="flex gap-3 justify-center">
+    <div className="flex gap-3 justify-center mb-3">
       <Link to="/">
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
           Accueil
